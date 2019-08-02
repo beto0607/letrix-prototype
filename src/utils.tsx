3fd01fb8fa4cd85a03886d9d81ed9bb14ff9a227
Roberto Albanesi <beto0607@gmail.comn>
2019-08-02 19:32:07 +0200

    Added function in utils for get a random item from an Array.

--- src/utils.tsx
+++ src/utils.tsx
@@ -7,6 +7,9 @@ export function shuffle<T>(a: Array<T>): Array<T> {
     }
     return a;
 }
+export function getRandomItem<T>(a: Array<T>): T {
+    return a[Math.floor(Math.random() * a.length)];
+}
 
 export function getLetters(words: Array<string>, shuffleResult: boolean = false): Array<string> {
     let ret: Array<string> = [];
